eb84844ca4817932a72119900560be777e055392
Rebecca <rebjmullin@gmail.com>
2019-09-29 13:07:42 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-field>
+      <md-input
+        v-model="currentTodo"
+        @keydown.enter="addTodo()"
+        placeholder="Add a todo"
+      />
+    </md-field>
+    <ul class="todos">
+      <li
+        :class="{ editing: todo.id === editedToDoId }"
+        v-for="todo in todos"
+        :key="todo.id"
+      >
+        <div class="md-card">
+          <div class="md-card-header md-card-header-flex">
+            <div class="md-display-3" @dblclick="toggleCompleted(todo)">
+              {{ todo.label }}
+            </div>
+          </div>
+
+          <div class="md-card-content">
+            <div class="md-checkbox">
+              <input
+                type="checkbox"
+                @change="toggleCompleted(todo)"
+                :checked="todo.completed"
+              />
+
+              <div class="md-checkbox-label">
+                <strong>completed :</strong>
+                <span class="">&nbsp;{{ todo.completed }}</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="md-card-actions md-alignment-right">
+            <button class="md-button md-raised" @click="removeTodo(todo)">
+              <span class="md-button__label">Delete</span>
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedToDoId: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    toggleCompleted(todo) {
+      todo.completed = !todo.completed;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.editing {
+  display: none;
 }
 </style>
