0254213541e632a15b67516fec5a0fbae8d6b34d
Rebecca <rebjmullin@gmail.com>
2019-09-29 13:16:59 -0400

    updating script

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:rebmullin/vue-todo.git master:gh-pages
 
 cd -
